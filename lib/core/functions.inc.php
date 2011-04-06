4440caec7150aa99c7922d484dba927b401960aa
Stan Rozenraukh <stan@jooonbug.com>
2011-04-06 16:06:31 -0400

    json_headers()

--- lib/core/functions.inc.php
+++ lib/core/functions.inc.php
@@ -222,6 +222,12 @@ if (!$common_inc_php) {
 
 	}//print_columns
 
+	function json_headers() {
+		header('Cache-Control: no-cache, must-revalidate');
+		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+		header('Content-type: application/json');
+	}
+
 
 
 /**
