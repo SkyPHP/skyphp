4888f38a74c4499919afa72648286f3af0362cf5
zgr024 <zach@joonbug.com>
2011-04-07 18:53:14 -0400

    tets

--- templates/html5/html5.js
+++ templates/html5/html5.js
@@ -256,23 +256,6 @@ function removeParam(url, param)
   return url;
 }
 
-function isNumeric(strString)
-   //  check for valid numeric strings	
-   {
-   var strValidChars = "0123456789";
-   var strChar;
-   var blnResult = true;
-
-   if (strString.length == 0) return false;
-
-   //  test strString consists of valid characters listed above
-   for (i = 0; i < strString.length && blnResult == true; i++)
-      {
-      strChar = strString.charAt(i);
-      if (strValidChars.indexOf(strChar) == -1)
-         {
-         blnResult = false;
-         }
-      }
-   return blnResult;
-   }
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
