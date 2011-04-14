7d7832b543b32e456ffae2ff49df5f6f5eccc45e
Will Schmid <will123195@gmail.com>
2011-04-14 13:40:54 -0400

    access group fix

--- sky.php
+++ sky.php
@@ -374,6 +374,7 @@ for ( $i=1; $i<=count($sky_qs); $i++ ) {
         else {
             $access_denied = true;
             // check duplicate logins to see if another person (with same user/pass) has access then undeny access
+            if ( is_array($rs_logins) )
             foreach ( $rs_logins as $person ) {
                 if ( auth_person( $access_groups, $person['person_id'] ) ) {
                     $access_denied = false;
