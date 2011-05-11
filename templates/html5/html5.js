6f7923cc19caff9eb035cb7d5b9abc5fc76681c9
Stan Rozenraukh <stan@jooonbug.com>
2011-05-11 11:18:25 -0400

    $.getCSS replaced

--- templates/html5/html5.js
+++ templates/html5/html5.js
@@ -174,18 +174,6 @@ $(function(){
         $('#overlay').fadeOut('slow');
     };
 
-    $.getCSS = function( url, callback ){
-        $.get(url,function(){
-            $(document.createElement('link')).attr({
-                href: url,
-                media: 'screen',
-                type: 'text/css',
-                rel: 'stylesheet'
-            }).appendTo('head');
-            if ( typeof callback == 'function' ) callback();
-        });
-    }
-
     jQuery.fn.center = function () {
         var top = ( $(window).height() - this.height() ) / 2+$(window).scrollTop();
         if ( top < 5 ) top = 5;
@@ -209,6 +197,119 @@ $(function(){
     }
 
 })( jQuery );
+
+(function (window, document, jQuery) {
+
+  var head = document.getElementsByTagName('head')[0],
+      loadedCompleteRegExp = /loaded|complete/,
+      callbacks = {},
+      callbacksNb = 0,
+      timer;
+
+  jQuery.getCSS = function (url, options, callback) {
+
+    if (jQuery.isFunction(options)) {
+      callback = options;
+      options  = {};
+    }
+
+    var link = document.createElement('link');
+
+    link.rel   = 'stylesheet';
+    link.type  = 'text/css';
+    link.media = options.media || 'screen';
+    link.href  = url;
+
+    if (options.charset) {
+      link.charset = options.charset;
+    }
+
+    if (options.title) {
+      callback = (function (callback) {
+        return function () {
+          link.title = options.title;
+          callback(link, "success");
+        };
+      })(callback);
+    }
+
+    // onreadystatechange
+    if (link.readyState) {
+
+      link.onreadystatechange = function () {
+        if (loadedCompleteRegExp.test(link.readyState)) {
+          link.onreadystatechange = null;
+          callback(link, "success");
+        }
+      };
+
+    // If onload is available, use it
+    } else if (link.onload === null /* exclude Webkit => */ && link.all) {
+      link.onload = function () {
+        link.onload = null;
+        callback(link, "success");
+      };
+
+    // In any other browser, we poll
+    } else {
+
+      callbacks[link.href] = function () {
+        callback(link, "success");
+      };
+
+      if (!callbacksNb++) {
+        // poll(cssPollFunction);
+
+        timer = window.setInterval(function () {
+
+          var callback,
+              stylesheet,
+              stylesheets = document.styleSheets,
+              href,
+              i = stylesheets.length;
+
+          while (i--) {
+            stylesheet = stylesheets[i];
+            if ((href = stylesheet.href) && (callback = callbacks[href])) {
+              try {
+                // We store so that minifiers don't remove the code
+                callback.r = stylesheet.cssRules;
+                // Webkit:
+                // Webkit browsers don't create the stylesheet object
+                // before the link has been loaded.
+                // When requesting rules for crossDomain links
+                // they simply return nothing (no exception thrown)
+                // Gecko:
+                // NS_ERROR_DOM_INVALID_ACCESS_ERR thrown if the stylesheet is not loaded
+                // If the stylesheet is loaded:
+                //  * no error thrown for same-domain
+                //  * NS_ERROR_DOM_SECURITY_ERR thrown for cross-domain
+                throw 'SECURITY';
+              } catch(e) {
+                // Gecko: catch NS_ERROR_DOM_SECURITY_ERR
+                // Webkit: catch SECURITY
+                if (/SECURITY/.test(e)) {
+
+                  // setTimeout(callback, 0);
+                  callback(link, "success");
+
+                  delete callbacks[href];
+
+                  if (!--callbacksNb) {
+                    timer = window.clearInterval(timer);
+                  }
+
+                }
+              }
+            }
+          }
+        }, 13);
+      }
+    }
+    head.appendChild(link);
+  };
+
+})(this, this.document, this.jQuery);
 
 function skybox_alert(text) {
     var html = '<div style="padding:10px;">';
