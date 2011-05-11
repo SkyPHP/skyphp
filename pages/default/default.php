0ef2caee491f524c27ca2914ff13c278643afbc1
Will Schmid <will123195@gmail.com>
2011-05-11 12:42:53 -0400

    default page

--- pages/default/default.php
+++ pages/default/default.php
@@ -2,15 +2,26 @@
 
 $p->title = 'Welcome to SkyPHP';
 
+$p->head = "
+<style>
+    body { padding: 10px; }
+    h1 { margin-bottom: 10px; font-size: 22px; }
+    h2 { margin-bottom: 10px; font-size: 15px; }
+</style>
+";
+
 $p->template('html5','top');
 ?>
+
 
 <h1><?=$p->title?></h1>
 
 <h2>Getting Started</h2>
 <p>
-    To replace this default homepage, create the following file in your codebase:<br />
+    To replace this default homepage, create the following file in your codebase:
+    <code>
     pages/default/default.php
+    </code>
 </p>
 
 <h2>Documentation</h2>
