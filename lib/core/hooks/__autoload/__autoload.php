d3d519decab75c656d9240515b1efc376593eaf9
Stan Rozenraukh <stan@stanistan.com>
2011-08-18 17:00:01 -0400

    autoload fix

--- lib/core/hooks/__autoload/__autoload.php
+++ lib/core/hooks/__autoload/__autoload.php
@@ -4,7 +4,7 @@ function __autoload($n) {
     aql::include_class_by_name($n);
     if (class_exists($n)) return;
     $file_path = 'lib/class/class.'.$n.'.php';
-    if (file_exsits_incpath($file_path)) {
+    if (file_exists_incpath($file_path)) {
 	   	include $file_path;
 	}
 }
