1e6cac87540329439af3c7e1661d9940e6384bd9
Stan Rozenraukh <stan@stanistan.com>
2011-09-13 12:12:28 -0400

    set flags on construction

--- lib/core/class.modelArrayObject.php
+++ lib/core/class.modelArrayObject.php
@@ -2,6 +2,13 @@
 
 class modelArrayObject extends ArrayObject {
 	
+	public function __construct() {
+		$args = func_get_args();
+		call_user_func_array(array('ArrayObject', __FUNCTION__), $args);
+		if ($this->getFlags()) return;
+		$this->setFlags(ArrayObject::ARRAY_AS_PROPS);
+	}
+
 	public function offsetSet($a, $b) {
 		if (is_array($b)) $b = new modelArrayObject($b);
 		return parent::offsetSet($a, $b);
