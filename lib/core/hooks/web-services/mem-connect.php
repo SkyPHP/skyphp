f6c860f36bb2d6cdd169c1cdd80e34054521df8f
Will Schmid <will123195@gmail.com>
2011-09-22 19:59:33 -0400

    redundant memcache support

--- lib/core/hooks/web-services/mem-connect.php
+++ lib/core/hooks/web-services/mem-connect.php
@@ -2,12 +2,16 @@
 
 // connect to memcached
 if ( class_exists('Memcache') && count($memcache_servers) ) {
+    if ( $memcache_redundancy ) {
+        ini_set('memcache.redundancy',$memcache_redundancy);
+        ini_set('memcache.session_redundancy',$memcache_redundancy);
+    }
     if (!$memcache_port) $memcache_port = 11211;
     $memcache = new Memcache;
     foreach ($memcache_servers as $memcache_host) {
         $memcache->addServer($memcache_host, $memcache_port);
         if ($memcache_save_path) $memcache_save_path .= ',';
-        $memcache_save_path .= $memcache_host . ':' . $memcache_port;
+        $memcache_save_path .= 'tcp://' . $memcache_host . ':' . $memcache_port;
     }
     if ( @$memcache->getVersion() == false ) $memcache = null;
 }
