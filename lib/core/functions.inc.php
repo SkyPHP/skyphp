1b253764f92175dd687c69ebfc6a6655905fde7d
Josh Hassara <josh.hassara@joonbug.com>
2011-10-05 19:41:25 -0400

    added regex to strip styles

--- lib/core/functions.inc.php
+++ lib/core/functions.inc.php
@@ -1301,4 +1301,10 @@ function krumo_debug_env(){
     
 }
 
+function strip_inline_style($string, $replacement_style = null) {
+	return preg_replace_callback('/\bstyle\="(?<style>[^"]*)"/', function($matches) use($replacement_style) {
+		return 'style="'.$replacement_style.'"';
+	}, $string);
+}
+
 ?>
