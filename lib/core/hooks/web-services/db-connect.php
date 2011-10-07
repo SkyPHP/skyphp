11dcb663fffdcb0d5c3b3e6a7f259e8a7141a8a3
John <john.bolger@joonbug.com>
2011-10-07 17:48:54 -0400

    fixed so that db read connections are distributed accross a replication cluster

--- lib/core/hooks/web-services/db-connect.php
+++ lib/core/hooks/web-services/db-connect.php
@@ -2,35 +2,55 @@
                   
 if ( !$db_host ) $db_host = $db_domain; // for backwards compatibility
 if ( $db_name && $db_host ) {
-   $db_hosts = explode(',', $db_host);
+   $db_hosts = is_array($db_host)?$db_host:explode(',', $db_host);
+
+   shuffle($db_hosts);
 
    $db_error = '';
 
    while($db_host = rtrim(trim(array_shift($db_hosts)))){
       # connect to read-only db
+
       $db = &ADONewConnection( $db_platform );
       @$db->Connect( $db_host, $db_username, $db_password, $db_name );
+
       if($db->ErrorMsg()) {
           $db_error .= "db error ($db_host): {$db->ErrorMsg()} \n";
       } else {
+         if($dbw){
+            break;
+         }
          # determine master db -- set $dbw_host
-         if($db_replication) include("lib/core/db-replication/{$db_replication}/{$db_replication}.php");
+         if($db_replication) include_once("lib/core/db-replication/{$db_replication}/{$db_replication}.php");
+
          if(!$dbw_host){ // we are not using replication
-            $dbw =& $db;
+            $dbw = &$db;
             $dbw_host = $db_host;
          }else { // we are using replication, connect to the the master db
-            $dbw = &ADONewConnection( $db_platform );
-            @$dbw->Connect( $dbw_host, $db_username, $db_password, $db_name );
-            // if we can't connect to master, then aql insert/update will
-            // gracefully fail and validation will display error message
-            if($dbw->ErrorMsg()) {
-               $master_db_connect_error = "<!-- \$dbw error ($dbw_domain): " . $dbw->ErrorMsg() . " -->";
-               $dbw = NULL;
+            if($dbw_host == $db_host){
+               $dbw = $db;
+               $db->Close();
+               unset($db);
+            }else{
+               $dbw = &ADONewConnection( $db_platform );
+               @$dbw->Connect( $dbw_host, $db_username, $db_password, $db_name );
+               // if we can't connect to master, then aql insert/update will
+               // gracefully fail and validation will display error message
+               if($dbw->ErrorMsg()) {
+                  $master_db_connect_error = "<!-- \$dbw error ($dbw_domain): " . $dbw->ErrorMsg() . " -->";
+                  $dbw = NULL;
+               }
             }
          }
-         #we have our choice of $db now, so we break
-         break;
+         if($db){
+            #we have our choice of $db now, so we break
+            break;
+         }
       }
+   }
+
+   if($dbw && !$db){
+      $db = &$dbw;
    }
 
    #if there is no $db_host, that means all our choices have failed
