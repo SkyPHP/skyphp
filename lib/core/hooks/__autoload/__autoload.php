e30a71e36427b21d0136744012a76453031958b8
Stan Rozenraukh <stan@stanistan.com>
2012-01-13 13:36:59 -0500

    use spl_autoload_register() instead of function __autoload, means we can have multiple autoloaders

--- lib/core/hooks/__autoload/__autoload.php
+++ lib/core/hooks/__autoload/__autoload.php
@@ -1,10 +1,21 @@
 <?
 
-function __autoload($n) {
-    aql::include_class_by_name($n);
-    if (class_exists($n)) return;
-    $file_path = 'lib/class/class.'.$n.'.php';
-    if (file_exists_incpath($file_path)) {
-	   	include $file_path;
+SkyPHP_Autoloader::Register();
+
+class SkyPHP_Autoloader {
+	
+	public static function Register() {
+		return spl_autoload_register(array('SkyPHP_Autoloader', 'Load'));
 	}
+
+	public static function Load($n) {
+		aql::include_class_by_name($n);
+	    if (class_exists($n)) return;
+	    $file_path = 'lib/class/class.'.$n.'.php';
+	    if (file_exists_incpath($file_path)) {
+		   	include $file_path;
+		}
+	}
+
 }
+
