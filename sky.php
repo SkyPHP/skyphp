851c0769d069f4b38b0164e480d2958e7b204934
John Bolger <john.bolger@joonbug.com>
2012-02-06 15:50:35 -0500

    added remember-uri hook, called uri

--- sky.php
+++ sky.php
@@ -480,13 +480,15 @@ define( 'XIDE', substr( $_SERVER['HTTP_REFERER'], strrpos($_SERVER['HTTP_REFERER
 
 // remember uri
 // if the page has a trailing '/' or '?', redirect to the remembered uri
-if ( strlen($p->uri) == strlen($p->urlpath) + 1 ) {
+/*if ( strlen($p->uri) == strlen($p->urlpath) + 1 ) {
     if ( $_SESSION['remember_uri'][$p->page_path] ) redirect($_SESSION['remember_uri'][$p->page_path]);
     else redirect($p->urlpath);
 // if the page has query folders and/or querystring, remember it
 } else if ( strlen($p->uri) > strlen($p->urlpath) + 1 ) {
     $_SESSION['remember_uri'][$p->page_path] = $p->uri;
 }
+*/
+include('lib/core/hooks/uri/uri.php');
 
 // if access denied, show login page
 if ( $access_denied ) {
