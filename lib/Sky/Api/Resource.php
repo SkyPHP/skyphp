040f195ad772de8efae1298b1afdb392e123f411
Will Schmid <will123195@gmail.com>
2012-05-14 14:40:53 -0400

    dateTimeArray formatting

--- lib/Sky/Api/Resource.php
+++ lib/Sky/Api/Resource.php
@@ -15,4 +15,25 @@ class Resource {
 		}
 	}
 
+	protected function dateArray($timestr) {
+		return $this->dateTimeArray($timestr, array('U', 'n-d-Y', 'l', 'F', 'n', 'd', 'S', 'Y'));
+	}
+
+	protected function timeArray($timestr) {
+		$values = $this->dateTimeArray($timestr, array('U', 'g:ia', 'g', 'i', 'a'));
+		if (is_array($values)) $values['formatted'] = str_replace(':00', '', $values['g:ia']);
+		return $values;
+	}
+
+	protected function dateTimeArray($timestr, $formats=null) {
+		if (!$timestr) return null;
+		$timestr = strtotime($timestr);
+		if (!is_array($formats)) $formats = array('U', 'n-d-Y g:ia', 'c', 'l', 'F', 'n', 'd', 'S', 'Y', 'g', 'i', 'a');
+		$data = array();
+		array_walk($formats, function($format, $key, $timestr) use(&$data){
+			$data[$format] = date($format, $timestr);
+		}, $timestr);
+		return $data;
+	}
+
 }
