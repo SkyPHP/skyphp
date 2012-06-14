e8fb7668639833f74c72d48b5afbc9924361c492
Will Schmid <will123195@gmail.com>
2012-06-14 14:14:59 -0400

    fix

--- lib/dev/sample_dev_index.php
+++ lib/dev/sample_dev_index.php
@@ -1,16 +1,23 @@
 <?php
 
+/**
+ * This is a sample index file for a dev site.
+ */
+
+// config
 # $down_for_maintenance = true;
 $dev_domain = 'example.com';
 $codebases_path = '/path/to/codebases/';
 $sites_json_file = 'sites.json';
+$skyphp_storage_path = '/home/skydevus/storage';
 
+$master_skyphp = $codebases_path . 'SkyPHP/skyphp/master/';
+include $master_skyphp . 'lib/dev/get-codebase.inc.php';
 
 $site_url = strtolower($_SERVER['HTTP_HOST']);
 $site = str_replace('.' . $dev_domain, '', $site_url);
-$master_skyphp = $codebases_path . 'SkyPHP/skyphp/master/';
 $sites = json_decode(file_get_contents($sites_json_file, true));
-include $master_skyphp . 'lib/dev/get-codebase.inc.php';
+
 $codebase_path_arr = array();
 if (is_array($sites->$site)) {
     foreach($sites->$site as $codebase) {
@@ -19,5 +26,5 @@ if (is_array($sites->$site)) {
 } else {
     $codebase_path_arr = array( $master_skyphp );
 }
-$skyphp_storage_path = "/home/skydevus/storage";
+
 include  end($codebase_path_arr) . 'sky.php';
