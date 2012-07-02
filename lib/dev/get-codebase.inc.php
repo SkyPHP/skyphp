3ed961918542352b0787c80cb78b97e5c3c9b58c
Will Schmid <will123195@gmail.com>
2012-07-02 02:21:40 -0400

    test

--- lib/dev/get-codebase.inc.php
+++ lib/dev/get-codebase.inc.php
@@ -55,6 +55,7 @@ function getCodeBase($codebase_path, $codebase)
             "cd $branch_path",
             "git clone -b $branch git@github.com:$user/$repository.git . 2>{$codebase_path}git.log"
         );
+
         echo safe_exec($cmds);
     }
 
