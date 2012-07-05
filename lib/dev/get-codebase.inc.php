58f34b9a117c49580eda53c95b6be7197a2379db
Will Schmid <will123195@gmail.com>
2012-07-05 11:51:46 -0400

    remove include

--- lib/dev/get-codebase.inc.php
+++ lib/dev/get-codebase.inc.php
@@ -1,7 +1,5 @@
 <?php
 
-include_once __DIR__ . '/../core/functions.inc.php';
-
 /**
  * Clones the repo from github if the branch folder does not exist
  * TODO: don't clone if the remote branch doesn't exist
