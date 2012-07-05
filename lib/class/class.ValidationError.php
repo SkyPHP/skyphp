3ace0d8dd0f525997356696eef428a49a3f8d9a9
Stan Rozenraukh <stan@stanistan.com>
2012-07-05 14:15:18 -0400

    formatting for ValidationError

--- lib/class/class.ValidationError.php
+++ lib/class/class.ValidationError.php
@@ -1,26 +1,27 @@
 <?php
 
 /**
- *  A standard error object to be used in validation
+ * A standard error object to be used in validation
+ * @package SkyPHP
  */
 class ValidationError
 {
 
     /**
-     *  The error code that uniquely identifieds this error
-     *  @var string
+     * The error code that uniquely identifieds this error
+     * @var string
      */
     public $code = '';
 
     /**
-     *  The message that belongs to this error
-     *  @var string
+     * The message that belongs to this error
+     * @var string
      */
     public $message = '';
 
     /**
-     *  @param  string  $error_code
-     *  @param  array   $params     additional properties of the error to be set
+     * @param string $error_code
+     * @param array  $params     additional properties of the error to be set
      */
     public function __construct($error_code, $params = array())
     {
@@ -35,28 +36,31 @@ class ValidationError
     }
 
     /**
-     *  If we are dev, each Validation Error should have its own trace
-     *  Sets $this->trace using a ValidationException
-     *  Only works if \Sky\Api::$is_dev is truthy
+     * If we are dev, each Validation Error should have its own trace
+     * Sets $this->trace using a ValidationException
+     * Only works if \Sky\Api::$is_dev is truthy
      */
     protected function setTrace()
     {
         if (!\Sky\Api::$is_dev) {
+
             return;
         }
 
         try {
             throw new ValidationException;
         } catch (ValidationException $e) {
-
-            # get trace -> array
-            $t = array_filter(preg_split('/\#\d+/', $e->getTraceAsString()));
-
-            # take off the first two pieces because they aren't necessary in the trace
-            array_shift($t);
-            array_shift($t);
-
-            $this->trace = $t;
+            // get trace -> array
+            // take off the first two pieces because they aren't necessary in the trace
+            $this->trace = array_slice(
+                array_filter(
+                    preg_split(
+                        '/\#\d+/',
+                        $e->getTraceAsString()
+                    )
+                ),
+                2
+            );
         }
     }
 
