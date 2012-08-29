7167cb8174a20a5f00dba39df7ee45f408ab85d7
Stan Rozenraukh <stan@stanistan.com>
2012-08-29 14:45:49 -0400

    initial classes

--- lib/Sky/Api/Identity.php
+++ lib/Sky/Api/Identity.php
@@ -5,35 +5,125 @@ namespace Sky\Api;
 abstract class Identity
 {
 
+    private $oauth_model = null;
+
     /**
      * Creates an Identity object based on params
      * @param array $params key/value pairs, usually:
      *      + person_id
      *      + app_key
      */
-    abstract public function __construct($params = null);
+    public function __construct($oauth = null)
+    {
+        $this->oauth_model = $oauth;
+    }
 
-    /**
-     * Gets the Identity associated with the specified oauth_token
-     * @param string $oauth_token
-     * @return Identity
-     * @abstract
-     */
-    abstract public static function get($oauth_token = null);
+    public static function get($token = null)
+    {
+        if (!$token) {
+            return new self(new $o);
+        }
 
-    /**
-     * Issues or retrieves an oauth_token
-     * @return string
-     * @abstract
-     */
-    abstract public function getOAuthToken();
+        $oauth = static::getOauthByToken($token);
+        if (!$oauth) {
+            static::error('Invalid Token.');
+        }
+
+        return new self($oauth);
+    }
+
+    abstract protected static function getOauthModel();
+
+    abstract protected static function getAppFieldName();
+
+    public static function getOauthByToken($token)
+    {
+        $model = static::getOauthModel();
+        return $model::getByToken($token);
+    }
+
+    public function getOAuthToken()
+    {
+        $person_id = $this->person_id();
+
+        if (!$person_id) {
+            static::error('User not specified.');
+        }
+
+        if (!\person::exists($person_id)) {
+            static::error('Invalid user.');
+        }
+
+        $clause = array(
+            'person_id' => $person_id
+        );
+
+        if (!$this->app_key()) {
+
+            $app_id = null;
+
+        } else {
+
+            $api_app = static::getApiAppByKey($this->app_key());
+
+            if (!$api_app) {
+                static::error('Invalid app_key.');
+            }
+
+            $app_id = $api_app->getID();
+            $clause['app_id'] = $app_id;
+        }
+
+        if (!$clause) {
+            static::error('Unknown Identity.');
+        }
+
+        $m = static::getOauthModel();
+        $oauth = $m::getOne($clause);
+
+        if (!$oauth || !$oauth->token) {
+            $oauth = $m::insert(array(
+                'person_id' => $person_id,
+                static::getAppFieldName() . '_id' => $app_id
+            ));
+        }
+
+        return array(
+            'oauth_token' => $oauth->token,
+            'issued' => $oauth->getTimeIssued(),
+            'now' => strtotime(\aql::now()),
+            'expires' => $oauth->getTimeExpires()
+        );
+    }
+
+    public function getModel()
+    {
+        return $this->oauth_model;
+    }
+
+    public function person_id()
+    {
+        return $this->getModel()->person_id;
+    }
+
+    public function app_key()
+    {
+        $f = static::getAppFieldName();
+
+        return $this->getModel()->$f->app_key;
+    }
+
+    public function isPublic()
+    {
+        return !$this->getModel();
+    }
 
     /**
      * Shorthand for throwing an exception
      * @param string $message error message
      * @throws \Exception
      */
-    protected function error($message)
+    protected static function error($message)
     {
         throw new \Exception($message);
     }
---
+++ lib/Sky/Api/Identity/Model/Oauth.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * Example AQL
+ *      sky_api_oauth {
+ *          sky_api_app_id,
+ *          mod_time,
+ *          person_id,
+ *          token,
+ *          [sky_api_app]
+ *      }
+ *      sky_api_app {
+ *
+ *      }
+ * @package SkyPHP
+ */
+abstract class Oauth extends \Model
+{
+    /**
+     * Setting for when
+     * @var string
+     */
+    public static $expiration_interval = '60 days';
+
+    /**
+     *  @var array
+     */
+    public $_required_fields = array(
+        'person_id' => 'Person',
+        'token' => 'Token'
+    );
+
+    /**
+     * Gets the field name of this model that this token will belong to
+     * IE: sky_api_app_id
+     * @return  string
+     */
+    abstract protected static function getAppIDFieldName();
+
+    /**
+     * @param  string  $token
+     * @return Model | null
+     */
+    public static function getByToken($token)
+    {
+        return (!is_string($token) || !$token)
+            ? null
+            : static::getOne(array('token' => $token));
+    }
+
+    /**
+     * Pre validate hook
+     * if insert and not provided with a token, generate one
+     */
+    public function beforeCheckRequiredFields()
+    {
+        if ($this->isInsert() && !$this->token) {
+            $this->token = $this->generateNewToken();
+        }
+    }
+
+    /**
+     * Generates oauth_token
+     * @return string
+     * @throws Exception
+     */
+    public function generateNewToken()
+    {
+        if (!$this->person_id) {
+            throw new \Exception('Unknown user.');
+        }
+
+        return encrypt($this->person_id . $this->ct_api_app_id . time());
+    }
+
+    /**
+     * @param  string  $format
+     * @return string | int
+     */
+    public function getTimeExpires($format = '')
+    {
+        $interval = '+' . static::$expiration_interval;
+        $time = strtotime($interval, $this->getTimeIssued());
+        return ($format) ? date($format, $time) : $time;
+    }
+
+    /**
+     *  @param  string  $format
+     *  @return string | int
+     */
+    public function getTimeIssued($format = '')
+    {
+        $time = strtotime($this->mod_time);
+        return ($format) ? date($format, $time) : $time;
+    }
+
+}
