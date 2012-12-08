f84ce982f429f5d8593b5eb28f4ed15d3ba4f487
hexxorleiva <hexxorleiva@gmail.com>
2012-12-07 21:30:21 -0500

    adding the latest google syntax for Google Analytics. Also includes a domain base so that all subdomains for cravetickets.com are tracked as well.

--- templates/html5/html5.php
+++ templates/html5/html5.php
@@ -107,11 +107,19 @@ if ( $template_area == 'top' ) {
 <?php
     if ( $google_analytics_account ) {
 ?>
-<script>
-    var _gaq=[['_setAccount','<?=$google_analytics_account?>'],['_trackPageview']]; // Change UA-XXXXX-X to be your site's ID
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
-    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g,s)}(document,'script'));
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '<?=$google_analytics_account?>']);
+  _gaq.push(['_setDomainName', 'cravetickets.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 </script>
 <?php
     }//google analytics
