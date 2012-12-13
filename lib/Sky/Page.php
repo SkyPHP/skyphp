52b2b1244d7c973dbff93785f096bede71b0fde4
will123195 <will123195@gmail.com>
2012-12-12 20:43:47 -0500

    reorder the css array because missing keys is interpreted as an object and javascript is expecting an array with incremental indexes

--- lib/Sky/Page.php
+++ lib/Sky/Page.php
@@ -432,7 +432,7 @@ class Page
         };
         $append = function($var, $key) use($p) {
             $p->$key = array_merge($p->$key, $var);
-            $p->$key = my_array_unique($p->$key);
+            $p->$key = array_values(my_array_unique($p->$key));
         };
 
         $map = array(
