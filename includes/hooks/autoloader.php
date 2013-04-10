678e0862d8c0020981402423732cc8671319a6b9
will123195 <will123195@gmail.com>
2013-04-10 14:41:08 -0400

    remove unnecessary

--- includes/hooks/autoloader.php
+++ includes/hooks/autoloader.php
@@ -3,4 +3,4 @@
 include('lib/Sky/Autoloader.php');
 spl_autoload_register(array('\Sky\Autoloader', 'namespaceLoader'));
 spl_autoload_register(array('\Sky\Autoloader', 'globalLoader'));
-//spl_autoload_register(array('\Sky\Autoloader', 'globalAqlModelLoader'));
+
