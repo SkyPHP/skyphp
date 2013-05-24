f999f33886debdf441017815a51ee364c72959a0
will123195 <will123195@gmail.com>
2013-05-24 16:21:11 -0400

    add token security for aql save backwards compatibility

--- pages/aql/save.php
+++ pages/aql/save.php
@@ -5,10 +5,24 @@
 
 $namespace = '\\MyApp\\Model\\';
 
-$model = $namespace . $this->queryfolders[0];
-
+$model_name = $this->queryfolders[0];
+$model = $namespace . $model_name;
+$primary_table = $model::getPrimaryTable();
 
 $o = new $model($_POST);
+
+// if this is an update, make sure the correct _token has been posted
+if ($o->isUpdate()) {
+	if ($_POST['_token'] != $o->getToken()) {
+		exit_json([
+			'status' => 'Error',
+			'errors' => [
+				'Invalid token.'
+			]
+		]);
+	}
+}
+
 $o->save();
 
 $response = $o->getResponse();
