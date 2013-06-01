f87001045ce64c8b7098da6efe8e3ac74cd9c444
will123195 <will123195@gmail.com>
2013-05-31 20:38:44 -0400

    test

--- lib/Sky/dev/get-codebase.inc.php
+++ lib/Sky/dev/get-codebase.inc.php
@@ -55,7 +55,7 @@ function getCodeBase($codebase_path, $codebase)
         $commands = array(
             'whoami',
             "cd {$branch_path}",
-            "sudo $git_path clone --recursive -b {$branch} git@github.com:{$user}/{$repository}.git ."
+            "$git_path clone --recursive -b {$branch} git@github.com:{$user}/{$repository}.git ."
         );
 
         $commands = array_map('escapeshellcmd', $commands);
