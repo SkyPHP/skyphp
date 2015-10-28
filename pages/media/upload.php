212f460495b53fddc09717005fa828ee482979a4
ebemagid <ernest.b@live.com>
2015-10-27 20:57:55 -0400

    added condition to save directly to model from media upload

--- pages/media/upload.php
+++ pages/media/upload.php
@@ -7,8 +7,9 @@
 	*		- $_POST = [
 	*			'folder' => 'full folder path that is needed', // REQUIRED
 	*			'redirectURL' => 'Current page that you're on (usually $this->uri)', // OPTIONAL
-	*			'filename' => 'desired file name', // OPTIONAL
+	*			'filename' => 'desired file name', // OPTIONAL (Use array of file name with the same name attribute as desired file name appended with '_cust')
 	*			'override' => '0 or 1', // OPTIONAL
+	*			'save' => '0 or 1', // OPTIONAL
 	*			'is_ajax_request' => '0 or 1' // OPTIONAL (Default to 0 [redirect with url variable containing json string] )
 	*		]
 	*   RETURN : JSON Encoded String via POST Method
@@ -16,7 +17,6 @@
 	use \Sky\skyMedia;
 
 	if($_POST && count($_POST) > 0 && $_FILES){
-
 		$data = [
 			'folder' => $_POST['folder'],
 			'files' => $_FILES,
@@ -26,14 +26,37 @@
 
 		$upload = skyMedia::fileUpload($data); // Already encoded to json
 		
-		if ($_POST['is_ajax_request']) {
-		    exit_json($response);
+		if ($_POST['is_ajax_request']){
+		    exit_json($upload);
+		} elseif($_POST['save']) {
+			// Find IDE in $_POST array and save object
+			foreach($_POST as $key => $val){
+				if(strpos($key, "_ide") !== false){
+					$namespace = '\\Crave\\Model\\';
+					$model_name = $namespace .  substr($key, 0, -4);
+					$model = new $model_name($_POST);
+
+					// Name function that handles media items in model saveMediaItems()
+					if(method_exists($model, "saveMediaItems")){
+						$model->json = $upload;
+						$model::saveMediaItems($model);
+					}
+
+					$url = $_POST['redirectURL'] ? $_POST['redirectURL'] : $_SERVER['HTTP_REFERER'];
+					
+					redirect($url);
+				}
+			}
 		} else {
+			// Return JSON string using URL Parameter
 			$url = $_POST['redirectURL'] ? $_POST['redirectURL'] : $_SERVER['HTTP_REFERER'];
 
 			$qs = '?return='.rawurlencode($upload);
 
 			redirect($url . $qs);
 		}
-	} 
+	} else {
+		// Handle case where files were not uploaded
+		throw new \Exception('No data/files were sent.  Could not upload.');
+	}
 ?>
